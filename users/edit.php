bf7579c5952df7485052695e4d81724c3ff4f249
ar1023 <ar1023.hz@gmail.com>
2016-04-25 09:34:47 +0800

    途中です。

--- users/edit.php
+++ users/edit.php
@@ -1,48 +1,33 @@
+
 <?php
 require('../dbconnect.php');
 require('../functions.php');
-
 // 仮のログインユーザーデータ
 $_SESSION['id'] = 1;
 $_SESSION['time'] = time();
-
 // ログイン判定
 if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time() ) {
     $_SESSION['time'] = time();
-
     $sql = sprintf('SELECT * FROM members WHERE id=%d',
         m($db, $_SESSION['id'])
     );
     $record = mysqli_query($db, $sql) or die(mysqli_error($db));
-
     // ログインしているのユーザーのデータ
     $member = mysqli_fetch_assoc($record);
 } else {
     header('Location: signin.php');
     exit();
 }
-
-
-
     // session_start();
-
     // $_SESSION['id'] = 1;
     // $_SESSION['time'] = time();
-
     // if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()
     //   $_SESSION['time'] = time();
-
-
-
           // $sql = 'SELECT * FROM `members` WHERE id = 1';
           // $members = mysqli_query($db, $sql) or die(mysqli_error($db));
           // $member = mysqli_fetch_assoc($members);
-
     // $_SESSION['users']=$_POST;
     // $_SESSION['users']['image']=$image
-
-
-
     // $error = array();
     // if (!empty($_POST)) { // フォーム送信時のみ処理
     //     // エラー項目の確認
@@ -51,7 +36,6 @@ if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time() ) {
     //         $error['nick_name'] = 'blank';
     //         // $error配列のnick_nameキーにblankという値を代入
     //     }
-
     //     if ($_POST['introduction'] == '') {
     //         $error['introduction'] = 'blank';
     //     }
@@ -93,18 +77,47 @@ if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time() ) {
     //         header('Location: check.php');
     //         exit();
     //     }
-
-
     // // 書き直し
     // if (isset($_REQUEST['action'])) {
     //   // $_REQUESTとは$_GET、$_POSTなどを保持するスーパーグローバル変数
     //   // $_REQUEST['action']が存在すればif文処理をする。
     //   if ($_REQUEST['action'] == 'rewrite') {
     //     $_POST = $_SESSION['join'];
-
     //     $error['rewrite'] = true;
     //   }
     // }
+
+        if (isset($_POST) && !empty($_POST)){
+
+                  // 画像をサーバーへアップロードする処理
+                  // 単に登録する画像の名前の文字列を他と絶対にかぶらない形で
+                  // 変数に代入する
+                  $image = date('YmdHis') . $_FILES['image']['name'];
+                  // date()関数とは、指定したフォーマットで現在の日時を返す
+                  // echo $image;
+
+                  move_uploaded_file($_FILES['image']['tmp_name'],
+                                     'member_picture/' . $image
+                                    );
+                
+                  // move_uploaded_file()関数とは、
+                  // 指定したファイルを指定した場所にアップロードする
+
+
+                  $sql = sprintf('UPDATE `members` SET `nick_name`="%s", `introduction`="%s", `picture_path`="%s" WHERE `id`=%d',
+                              mysqli_real_escape_string($db,$_POST['nick_name']),
+                              mysqli_real_escape_string($db,$_POST['introduction']),
+                              mysqli_real_escape_string($db,$image),
+                              mysqli_real_escape_string($db, $_SESSION['id'])
+                          );
+
+                  mysqli_query($db,$sql) or die(mysqli_error($db));
+
+                  // check.phpに遷移して処理を終了する
+                  // header('Location: index.php');
+                  // exit();
+                }
+                
 
 ?>
 
@@ -117,6 +130,8 @@ if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time() ) {
   <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css"> 
   <link rel="stylesheet" type="text/css" href="../assets/css/main.css">
   <link rel="stylesheet" type="text/css" href="../assets/css/sample.css">
+  <link rel="stylesheet" href="http://black-flag.net/data/css/reset.css">
+  <link rel="stylesheet" href="css/common.css">
 
 </head>
 <body>
@@ -150,7 +165,7 @@ if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time() ) {
         <ul class="nav navbar-nav navbar-right">
           <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                <span class="glyphicon glyphicon-user"></span> 
+                <span class="glyphicon glyphicon-user"></span> 
                 <strong>nick_name</strong>
                 <span class="glyphicon glyphicon-chevron-down"></span>
             </a>
@@ -192,25 +207,15 @@ if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time() ) {
     </div>
     </div>
 
-<!--     <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-    <div class="container">
-      <div class="fb-profile">
-        <img align="left" class="fb-image-profile thumbnail" src="../member_picture/<?php echo $member['picture_path']; ?>" alt="Profile image example"/>
-        <div class="fb-profile-text"></div>
-      </div>
-    </div> -->
-  <!--   <div id='boxImage'>ここに画像が入ります</div> -->
-  <br>
-<form action="#" method="post" enctype="multipart/form-data">
-  <label>
-    <input type="checkbox" name="checked" value="1" checked="checked" />
-    <small>現在の画像を使用</small><br />
-  </label>
-  <label>
-    <img src="../member_picture/<?php echo $member['picture_path']; ?>" class="uploaded thumb" alt="" /><br />
-    <input type="file" name="images" />
-  </label>
-</form>
+
+<div id="container">
+  <div class="imgInput">
+      <img src="member_picture/<?php echo $member['picture_path']; ?>" alt="" class="imgView" width="100px" height="100px"><br>
+          <input type="file" name="image"><br>
+  </div><!--/.imgInput-->
+</div>
+
+
 
  <!-- /container -->
 
@@ -226,11 +231,11 @@ if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time() ) {
 
     <!-- Form Name -->
     <legend>My Profile Setting</legend>
-
+<form action="" method="post">
       <div class="form-group">
       <label class="col-md-4 control-label" for="textinput">Name</label>  
       <div class="col-md-4">
-      <input id="textinput" name="textinput" type="text" placeholder="your new name " class="form-control input-md" value="<?php echo $member['nick_name']; ?>">  
+      <input id="textinput" name="nick_name" type="text" placeholder="your new name " class="form-control input-md" value="<?php echo $member['nick_name']; ?>">  
       </div>
     </div>
    
@@ -239,7 +244,7 @@ if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time() ) {
 <div class="form-group">
   <label class="col-md-4 control-label" for="selectmultiple">About Me</label>
   <div class="col-md-4">
-    <textarea name="selectmultiple" class="form-control" multiple="multiple" placeholder="About me"><?php echo $member['introduction']; ?>
+    <textarea name="introduction" class="form-control" multiple="multiple" placeholder="About me"><?php echo $member['introduction']; ?>
     
     </textarea>
   </div>
@@ -249,9 +254,10 @@ if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time() ) {
     <div class="form-group">
       <label class="col-md-4 control-label" for="singlebutton"></label>
       <div class="col-md-4">
-        <a href="check.php" name="singlebutton" class="btn btn-info pull-right" >save</a>
+        <a href="user_index.php" name="singlebutton" class="btn btn-info pull-right" >save</a>
       </div>
     </div>
+</form>
 
     <!-- Button -->
    <!--  <div class="form-group">
@@ -288,5 +294,40 @@ if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time() ) {
 <script type="text/javascript" src="../assets/js/bootstrap.js"></script>
 <!-- jQueryファイルの次にbootstrapのJSファイル -->
 <script type="text/javascript" src="../assets/js/main.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script>
+$(function(){
+    var setFileInput = $('.imgInput'),
+    setFileImg = $('.imgView');
+
+    setFileInput.each(function(){
+        var selfFile = $(this),
+        selfInput = $(this).find('input[type=file]'),
+        prevElm = selfFile.find(setFileImg),
+        orgPass = prevElm.attr('src');
+
+        selfInput.change(function(){
+            var file = $(this).prop('files')[0],
+            fileRdr = new FileReader();
+
+            if (!this.files.length){
+                prevElm.attr('src', orgPass);
+                return;
+            } else {
+                if (!file.type.match('image.*')){
+                    prevElm.attr('src', orgPass);
+                    return;
+                } else {
+                    fileRdr.onload = function() {
+                        prevElm.attr('src', fileRdr.result);
+                    }
+                    fileRdr.readAsDataURL(file);
+                }
+            }
+        });
+    });
+});
+</script>
 </body>
 </html>
